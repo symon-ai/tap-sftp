e4764da2b075fa38551547439d3a7cb6f2322f19
Mohammed <rexmecse@gmail.com>
2022-08-02 20:12:13 -0600

    WP-1182 Exception for importing gpg key and decryption error

--- tap_sftp/decrypt.py
+++ tap_sftp/decrypt.py
@@ -7,7 +7,9 @@ LOGGER = singer.get_logger()
 
 def gpg_decrypt_to_file(gpg, src_file_path, decrypted_path, passphrase):
     with open(src_file_path, 'rb') as file_obj:
-        gpg.decrypt_file(file_obj, output=decrypted_path, passphrase=passphrase)
+        decryption_result = gpg.decrypt_file(file_obj, output=decrypted_path, passphrase=passphrase)
+        if decryption_result.returncode in [1, 2]:
+            raise Exception(f'There was an error while decrypting file. Please verify decryption settings are correct.')
     return decrypted_path
 
 
@@ -22,7 +24,9 @@ def initialize_gpg(key, gnupghome):
         except OSError:
             raise Exception(f'Unable to create GNU home directory at "{gnupghome}"')
     gpg = gnupg.GPG(gnupghome=gnupghome)
-    gpg.import_keys(key)
+    import_key_result = gpg.import_keys(key)
+    if import_key_result.returncode != 0:
+        raise Exception(f"Unable to import decryption key. Please verify the provided key is correct.")
     return gpg
 
 
@@ -46,5 +50,8 @@ def gpg_decrypt_from_remote(src_file_object, source_file_path, out_dir, key, gnu
 def gpg_decrypt_with_capturer(src_file_object, key, gnupghome, passphrase, capturer):
     gpg = initialize_gpg(key, gnupghome)
     gpg.on_data = capturer
-    gpg.decrypt_file(src_file_object, always_trust=True, passphrase=passphrase)
+    decryption_result = gpg.decrypt_file(src_file_object, always_trust=True, passphrase=passphrase)
+    if decryption_result.returncode in [1, 2]:
+        raise Exception(f'There was an error while decrypting file. Please verify decryption settings are correct.')
+
     return capturer.out_file_path
