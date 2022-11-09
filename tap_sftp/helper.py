bc78fffe749d3c273d9bc7b5d409f4b209f16b9f
ChrisLing1 <cling@varicent.com>
2022-11-09 10:06:25 -0500

    change aws_ssm.get_decryption_key to .get_parameter_value

--- tap_sftp/helper.py
+++ tap_sftp/helper.py
@@ -14,16 +14,19 @@ LOGGER = singer.get_logger()
 
 
 def update_decryption_key(decryption_configs):
-    storage_type = decryption_configs.get('key_storage_type', 'AWS_Secrets_Manager')
+    storage_type = decryption_configs.get(
+        'key_storage_type', 'AWS_Secrets_Manager')
     LOGGER.info(f'Using key storage type "{storage_type}"')
     if storage_type == "AWS_SSM":
-        decryption_configs['key'] = AWS_SSM.get_decryption_key(decryption_configs.get('key_name'))
+        decryption_configs['key'] = AWS_SSM.get_parameter_value(
+            decryption_configs.get('key_name'))
 
     elif storage_type == "AWS_Secrets_Manager":
         secret_manager = AWSSecretsManager(os.environ.get('AWS_REGION'))
         secret = secret_manager.get_secret(decryption_configs.get('key_name'))
         secret_json = json.loads(secret)
-        decryption_configs['key'] = base64.b64decode(secret_json['privateKeyEncoded'])
+        decryption_configs['key'] = base64.b64decode(
+            secret_json['privateKeyEncoded'])
         decryption_configs['passphrase'] = secret_json['passphrase']
 
 
